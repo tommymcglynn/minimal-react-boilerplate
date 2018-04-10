47a8b62d48747b43f910c5938072eb2a51e7b0aa
Tommy McGlynn <tmcglynn@apple.com>
2018-04-10 16:25:33 -0700

    babel presets

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,7 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
-                    }
-                }
+                use: 'babel-loader'
             }
         ]
     },
